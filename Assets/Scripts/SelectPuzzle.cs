2b4c79bf4a1a93233e107b01c05ad09e9c0acb44
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-07 15:36:34 +0100

    Create settings menu and create MenuController

---
+++ Assets/Scripts/SelectPuzzle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class SelectPuzzle : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject panel;
+	[SerializeField]
+	private Animator anim;
+	private string selectedPuzzle;
+
+	public void SelectedPuzzle () {
+		selectedPuzzle = EventSystem.current.currentSelectedGameObject.name;
+		Debug.Log (selectedPuzzle);
+	}
+
+}
---
+++ Assets/Scripts/SettingsController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingsController : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject panel;
+	[SerializeField]
+	private Animator anim;
+
+	public void OpenSettings () {
+		panel.SetActive (true);
+		anim.Play ("settingsPanelSlideIn");
+	}
+
+	public void CloseSettings () {
+		StartCoroutine (CloseSettingsPanel ());
+	}
+
+	private IEnumerator CloseSettingsPanel () {
+		anim.Play ("settingsPanelSlideOut");
+		yield return new WaitForSeconds (0.6f);
+		panel.SetActive(false);
+	}
+}
