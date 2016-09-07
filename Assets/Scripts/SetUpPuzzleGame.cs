7fe0abb07ff8102aeb71e0370999b30ee22e97e9
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-07 19:58:12 +0100

    Add card sprites at runtime

---
+++ Assets/Scripts/SetUpPuzzleGame.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SetUpPuzzleGame : MonoBehaviour {
+
+	private Sprite[] candyPuzzleSprites, transportPuzzleSprites, fruitPuzzleSprites;
+	private List<Sprite> gamePuzzles = new List<Sprite>();
+	private List<Button> puzzleButtons = new List<Button>();
+	private List<Animator> puzzleButtonAnimators = new List<Animator>();
+	private int level;
+	private string selectedPuzzle;
+	private int looper;
+
+	private void Awake () {
+		candyPuzzleSprites = Resources.LoadAll<Sprite> ("Sprites/Candy");
+		transportPuzzleSprites = Resources.LoadAll<Sprite> ("Sprites/Transport");
+		fruitPuzzleSprites = Resources.LoadAll<Sprite> ("Sprites/Fruits");
+	}
+
+	public void SetLevelAndPuzzle (int level, string selectedPuzzle) {
+
+	}
+
+	public void SetPuzzleButtonAndAnimators (List<Button> puzzleButtons, List<Animator> puzzleButtonAnimators) {
+
+	}
+
+	private void PrepareGameSprites () {
+
+	}
+
+	private void Shuffle (List<Sprite> list) {
+
+	}
+}
